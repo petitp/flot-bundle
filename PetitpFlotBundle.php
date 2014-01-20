3ceeec787dfdea830d570e45a05568e81b829f6c
Andrej Hudec <andrej.hudec@smeonline.sk>
2014-01-20 08:09:15 +0100

    Added flot library 0.8.2

---
+++ PetitpFlotBundle.php
@@ -0,0 +1,16 @@
+<?php
+/*
+* This file is part of the PetitpFlotBundle.
+
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Petitp\Bundle\FlotBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SonatajQueryBundle extends Bundle
+{
+
+}
